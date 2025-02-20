02845627d8e7e89fdbdb3b99a171af2e49be102a
Jean Haro <JeanCHL98@hotmail.com>
2025-02-20 00:35:52 -0500

    111 - CRON Tasks

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,5 +1,22 @@
+import { CronJob } from 'cron';
+
 export class Server {
     static start() {
         console.log('Server started...');
+
+        const job = new CronJob(
+            // Se coloco para 2 segundos
+            '*/2 * * * * *', // cronTime
+            () => {
+                // Se ejecutara cada 2 segundos
+                const date = new Date();
+                console.log('2 second', date);
+            }, // onTick
+            null, // onComplete
+            true, // start
+            'America/Los_Angeles' // timeZone
+        );
+
+        job.start();
     }
 }
